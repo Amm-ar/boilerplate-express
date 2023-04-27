9cdbf5155cb6d6eb41af92f828e8e62042b74a60
Amm-ar <amar9683@outlook.com>
2023-04-27 09:34:38 +0200

    created a route - /now - with a chained middleware

--- myApp.js
+++ myApp.js
@@ -25,5 +25,13 @@ app.get("/json", function(req, res){
     }
 });
 
+app.get("/now", function(req, res, next){
+    req.time = new Date().toString();
+    next();
+},
+function(req, res){
+    res.send({"time": req.time});
+});
+
 
  module.exports = app;
